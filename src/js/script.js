b173762d628db74cf1f6fa6ea1630f072479514f
Damian Serrano Thode <dsthode@gmail.com>
2015-02-18 21:18:37 +0100

    Changes

--- src/js/script.js
+++ src/js/script.js
@@ -18,9 +18,21 @@ $(document).ready(function() {
 	$('#color-picker').change(function(ev) {
 		try {
 			var tmp = hexToRGB($('#color-picker').spectrum('get').toHexString());
-			opts.red = Math.round(tmp.red/10) * 10;
-			opts.green = Math.round(tmp.green/10) * 10;
-			opts.blue = Math.round(tmp.blue/10) * 10;
+			var tmp1 = Math.round(tmp.red/10) * 10;
+			if (tmp1 > 250) {
+				tmp1 = 250;
+			}
+			opts.red = tmp1;
+			tmp1 = Math.round(tmp.green/10) * 10;
+			if (tmp1 > 250) {
+				tmp1 = 250;
+			}
+			opts.green = tmp1;
+			tmp1 = Math.round(tmp.blue/10) * 10;
+			if (tmp1 > 250) {
+				tmp1 = 250;
+			}
+			opts.blue = tmp1;
 			updateBadgeCode();
 		} catch (err) {}
 	});
@@ -46,10 +58,19 @@ function updateBadgeCode() {
 			opts.formato + '/' + opts.ancho + '/linkeo_con_respeto_' + 
 			opts.formato + '_' + opts.red + '_' + 
 			opts.green + '_' + opts.blue + '.png';
-		var badgeCode = '<a href="http://www.linkeoconrespeto.com" target="_blank">' +
+		var badgeCode = '<a href="http://www.linkeoconrespeto.com" target="_blank" rel="nofollow">' +
 			'<img src="' + imgSrc + '" width="220" height="' + 
 			(opts.formato == 'cuadrado' ? 220 : 62) + '" alt="Logo de Linkeo con Respeto"></a>';
 		$('#badge-code').val(badgeCode);
 		$('#logo-color-pick').attr('src', imgSrc);
 	}
 };
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-59813409-1', 'auto');
+ga('send', 'pageview');
+
